a7be4867dffe62f27551df04c86de810f5ec9d0f
vicsn <vicsn@users.noreply.github.com>
2019-08-31 19:51:24 +0200

    use Result

--- src/main.rs
+++ src/main.rs
@@ -10,42 +10,56 @@
 //! 
 
 
+/// Evaluate x**3 + x + 5
+///
+/// # arguments
+///
+/// * `x` - an integer
+/// 
+/// # return value
+///
+/// * x**3 + x + 5
 fn qeval(x: i32) -> i32 {
     let y = x.pow(3);
     y + x + 5
 }
 
-// The first step is a “flattening” procedure, where we convert the original code, which may
-// contain arbitrarily complex statements and expressions, into a sequence of statements that are
-// of two forms:
-// (1) x = y (where y can be a variable or a number)
-// (2) x = y (op) z (where op can be +, -, *, / and y and z can be variables, numbers or themselves
-// sub-expressions)
-// 
-// [(multiplicant,power)]
 
-///
-/// Flatten an equation
+/// We convert the original code, which may contain arbitrarily complex statements and expressions,
+/// into a sequence of statements that are of two forms:
+/// (1) x = y (where y can be a variable or a number)
+/// (2) x = y (op) z (where op can be +, -, *, / and y and z can be variables, numbers or themselves
+/// sub-expressions)
 ///
 /// # arguments
 ///
-/// * `equation` - the polynomial equation to flatten, represented as a Vec<i32>
+/// * `equation` - the polynomial equation to flatten, represented as a Vec<(multicpliant,power)>
 /// 
 /// # return value
 ///
-/// The flattened equation
-pub fn flattening(equation: std::vec::Vec<(i32, i32)>) -> std::vec::Vec<(i32, i32)> {
-//     for element in equation.iter() {
-//         if (element == 2) {
-//             // element.1;
-//         }
-        let tmp: std::vec::Vec<(i32, i32)> = vec![(1,5)];
-        tmp
+/// * The flattened equation
+pub fn flattening(equation: std::vec::Vec<(i32, i32)>) -> Result<std::vec::Vec<(i32, i32)>, std::string::String> {
+    let mut flattened: std::vec::Vec<(i32, i32)> = vec![];
+    for &element in equation.iter() {
+        // negative multiplicands or powers are not allowed
+        if element.0 < 1 || element.1 < 1 {
+            // Err(std::error::Error)
+            // Err(std::string::String::from("Error Happens!"))
+            // Err(())
+            ()
+        }
+        if element.0 != 1 {
+            flattened.push(element);
+        }
+        if element.1 == 2 {
+            flattened.push(element);
+        }
 //         // let sym_1 = x * x
 //         // let y = sym_1 * x
 //         // let sym_2 = y + x
 //         // let out = sym_2 + 5
-//     }
+    }
+    Ok(flattened)
 }
 
 fn main() {
@@ -59,9 +73,15 @@ fn main() {
     let equations: std::vec::Vec<std::vec::Vec<(i32, i32)>> = vec![vec![(1,3),(1,1),(5,0)]];
     let mut flattened = Vec::new();
     for equation in equations.iter() {
-        flattened.push(flattening(equation.to_vec()));
-        for element in equation.iter() {
-            println!("{}x**{}", element.0, element.1);
+        let r = flattening(equation.to_vec());
+        match r.clone() { // TODO is there another way to do this without cloning?
+            Ok(value) => {
+                    for element in value.iter() {
+                        println!("{}x**{}", element.0, element.1);
+                    }
+                },
+            Err(error) => println!("{}", error),
         }
+        flattened.push(r);
     }
 }
